420f4eed328fabaf50382b573a950a97bd5ddae4
Nikolai German <129080480+NikoGerman@users.noreply.github.com>
2024-11-04 14:00:49 +0100

    Q1 and parts of Q2

--- R/Q1.R
+++ R/Q1.R
@@ -1,66 +1,37 @@
-##### Question 1 ######
-##### Electricity and national income #####
-# How does access to electricity correlate with adjusted net national income
-# per capita across different countries? Does this also depend on the country
-# size or the inhabitants?
-#########################
 library(dplyr)
+library(tidyr)
 library(ggplot2)
-
-Worldbank <- readr::read_rds("Data/Worldbank.RDS")
-
-summary(Worldbank)
-str(Worldbank)
-
-Worldbank %>%
-  filter(is.na(`Access to electricity (% of population)`))
-
-Worldbank_Q1 <- Worldbank %>%
-  filter(Year < 2023) %>%
-  select("Country Name":"Adjusted net national income per capita (current US$)", "Surface area (sq. km)", "Population, total")
-    
-
-Worldbank_Q1 %>%
+# load cleaned Worldbank Data, discard 2022 and 2023 observations, since
+# the variables we are interested in, are incomplete in these two years.
+Worldbank_Q1 <- readr::read_rds("Data/cleaned/Worldbank.RDS") %>%
   filter(Year < 2022) %>%
-  ggplot(aes(x = `Access to electricity (% of population)`, y = `Adjusted net national income per capita (current US$)`)) +
-  geom_point(color = "blue", alpha = .5, shape = "diamond", size = 2)+
-  scale_y_log10()+
-  facet_grid(rows = vars(Year))+
-  theme_light()
-
-Worldbank_Q1 %>%
-  filter(Year < 2022) %>%
-  ggplot(aes(x = `Access to electricity (% of population)`, y = `Adjusted net national income per capita (current US$)`)) +
-  geom_point(color = "blue", alpha = .5, shape = "diamond", size = 2)+
-  geom_line()+
-  scale_y_log10()+
-  facet_grid(cols = vars(`Country Code`))+
-  theme_light()
-
-Worldbank_Q1 %>%
-  filter(Year == 2021) %>%
-  ggplot(aes(x = `Access to electricity (% of population)`, y = `Adjusted net national income per capita (current US$)`)) +
-  geom_point(color = "blue", alpha = .5, shape = "diamond", size = 2)+
-  scale_y_log10()+
-  theme_light()
-
-Worldbank_Q1 %>%
-  mutate(Electricity_75 = `Access to electricity (% of population)`>= 75) %>%
-  ggplot(aes(y = `Adjusted net national income per capita (current US$)`, x = Electricity_75))+
-  geom_boxplot() +
-  scale_y_log10()
+  select("Country Name":"Adjusted net national income per capita (current US$)", 
+         "Surface area (sq. km)", "Population, total", "Population density (people per sq. km of land area)", "Pop.Index2014")
 
 Plot_Electricity1 <- Worldbank_Q1 %>%
   ggplot(aes(y = `Adjusted net national income per capita (current US$)`, x = cut_interval(`Access to electricity (% of population)`, n = 4, breaks = c(0,25,50,75,100), labels = c("0 - 25%", "25 - 50%", "50 - 75%", "75 - 100%"))))+
   geom_point(aes(colour = `Country Code` == 'AFG')) +
   geom_boxplot()+
   scale_y_log10() +
-  labs(color = "Country")+
+  labs(x = "Access to electricity",
+       y = "Net national income per capita",
+       color = "Country",
+       title = "National income vs Access to Electricity",
+       caption = "x: Access to Electricity in %s\ny: Adjusted Net national income per capita in current USD") +
   scale_color_manual(labels = c("Rest of World", "Afghanistan"), values = c("darkgrey", "red"))+
-  ylab("Adj. net national income p.c. (current US$)")+
-  xlab("Access to electricity")+
   theme_light()
 ggsave("Plots/Q1-1.png", Plot_Electricity1, device = "png")
+
+Worldbank_Q1 %>%
+  ggplot(aes(y = `Adjusted net national income per capita (current US$)`,
+             x = cut_interval(`Access to electricity (% of population)`, n = 4, breaks = c(0,25,50,75,100), labels = c("0 - 25%", "25 - 50%", "50 - 75%", "75 - 100%"))))+
+  geom_boxplot() +
+  geom_point(data = Worldbank_Q1 %>% filter(`Country Code` == "AFG"), color = "red") +
+  scale_y_log10() +
+  #scale_color_manual(labels = c("Rest of World", "Afghanistan"), values = c("darkgrey", "red"))+
+  ylab("Adj. net national income p.c. (current US$)") +
+  xlab("Access to electricity") +
+  theme_light()
 
 Plot_Electricity_wo_AFG <- Worldbank_Q1 %>%
   filter(`Country Code` != "AFG") %>%
@@ -71,19 +42,12 @@ Plot_Electricity_wo_AFG <- Worldbank_Q1 %>%
   xlab("Access to electricity")+
   theme_light()
 
-#cut_interval(Worldbank_Q1$`Access to electricity (% of population)`,n = 4, breaks = c(0,25,50,75,100), labels = c("(0,25]", "(25,50]", "(50,75]", "(75,100]"))
-
 Worldbank_Q1 %>%
   group_by(`Country Name`) %>%
-  summarize(tau = cor(x = `Access to electricity (% of population)`, 
+  summarize(r_pearson = cor(x = `Access to electricity (% of population)`, 
                         y = `Adjusted net national income per capita (current US$)`,
                         method = "pearson",
                         use = "na.or.complete"))
-
-Worldbank_Q1 %>%
-  filter(`Country Code` == "CZE") %>%
-  ggplot(aes(y = `Adjusted net national income per capita (current US$)`, x = `Access to electricity (% of population)`))+
-  geom_point()
 
 low_elec <- Worldbank_Q1 %>%
   group_by(`Country Code`) %>%
@@ -98,7 +62,88 @@ Worldbank_Q1 %>%
   geom_smooth(method = "lm", se = FALSE, color = "grey")+
   geom_smooth(aes(group = `Country Code`, color = `Country Name`), method = "lm", se = FALSE)+
   scale_y_log10()+
-  scale_color_brewer(type = "qual")
-  ylab("Adj. net national income p.c. (current US$)")+
-  xlab("Access to electricity")+
+  scale_color_brewer(type = "qual")+
+  labs(x = "Access to electricity",
+       y = "Net national income per capita",
+       color = "Country",
+       title = "National income vs Access to Electricity",
+       caption = "x: Acces to Electricity in %s\ny: Adjusted Net national income per capita in current USD") +
+  theme_light()
+
+########### AREA
+Worldbank_Q1 %>%
+  mutate(`Surface area (million sq. km)` = `Surface area (sq. km)` / 1000000) %>%
+  ggplot(aes(x = `Surface area (million sq. km)`, y = `Adjusted net national income per capita (current US$)`)) +
+  geom_point(aes(color = `Country Name`)) +
+  theme_light() +
+  labs(x = "Area",
+       y = "Net National income per capita",
+       color = "Country",
+       title = "National income vs Area",
+       caption = "x: Surface Area in Million square km\ny: Adjusted Net national income per capita in current USD") +
+  scale_y_log10() +
+  scale_x_sqrt()
+
+########### POPULATION
+Worldbank_Q1 %>%
+  filter(`Country Code` != "HKG") %>%
+  ggplot(aes(x = `Population, total`, color = `Country Name`)) +
+  geom_point(aes(y = `Adjusted net national income per capita (current US$)`)) +
+  theme_light() +
+  labs(x = "",
+           y = "",
+           color = "Country",
+           title = "National income vs Population",
+           caption = "x: Population in Million Residents\ny: Adjusted Net national income per capita in current USD") +
+  scale_y_log10()
+
+Worldbank_Q1 %>%
+  filter(`Country Code` != "HKG") %>%
+  ggplot(aes(x = `Population, total`, color = `Country Name`)) +
+  geom_point(aes(y = `Adjusted net national income per capita (current US$)`)) +
+  theme_light() +
+  labs(x = "",
+       y = "",
+       color = "Country",
+       title = "National income vs Population",
+       caption = "x: Population in Million Residents\ny: Adjusted Net national income per capita in current USD") +
+  facet_wrap(vars(`Country Name`), scales = "free") +
+  guides(color = "none")
+########### Population Indexed by 2014
+Worldbank_Q1 %>%
+  filter(`Country Code` != "HKG") %>%
+  ggplot(aes(x = Pop.Index2014, color = `Country Name`)) +
+  geom_point(aes(y = `Adjusted net national income per capita (current US$)`)) +
+  theme_light() +
+  labs(x = "",
+       y = "",
+       color = "Country",
+       title = "National income vs Population",
+       caption = "x: 100 = Population in the Year 2014\ny: Adjusted Net national income per capita in current USD") +
+  scale_y_log10()
+
+Worldbank_Q1 %>%
+  filter(`Country Code` != "HKG") %>%
+  ggplot(aes(x = Pop.Index2014, color = `Country Name`)) +
+  geom_point(aes(y = `Adjusted net national income per capita (current US$)`)) +
+  theme_light() +
+  labs(x = "",
+       y = "",
+       color = "Country",
+       title = "National income vs Population growth",
+       caption = "x: Population Indexed to 2014 levels\ny: Adjusted Net national income per capita in current USD") +
+  facet_wrap(vars(`Country Name`), scales = "free_y") +
+  guides(color = "none")
+########### POPULATION-DENSITY
+Worldbank_Q1 %>%
+  filter(`Country Code` != "HKG")  %>%
+  ggplot(aes(x = `Population density (people per sq. km of land area)`, y = `Adjusted net national income per capita (current US$)`, colour = `Country Name`))+
+  geom_point() +
+  labs(y = "Net national income per capita",
+       x = "Population Density",
+       color = "Country",
+       title = "National income vs Population Density",
+       caption = "x: Residents per square km\ny: Adjusted Net national income per capita in current USD") +
+  scale_y_log10() + 
+  scale_x_log10() +
   theme_light()
--- R/ReadingData.R
+++ R/ReadingData.R
@@ -3,6 +3,7 @@ library(tidyr)
 Worldbank1_raw <- readxl::read_excel("Data/raw/Worldbank1.xlsx")
 Worldbank2_raw <- readxl::read_excel("Data/raw/Worldbank2.xlsx")
 
+# no usefull data after row 208
 Worldbank1 <- Worldbank1_raw[1:208,] %>%
   mutate(across(`2023 [YR2023]`:average, as.numeric)) %>%
   select(-average, -`Series Code`) %>%
@@ -10,6 +11,7 @@ Worldbank1 <- Worldbank1_raw[1:208,] %>%
   mutate(Year = as.numeric(substr(Year, start = 1, stop = 4))) %>%
   pivot_wider(names_from = "Series Name", values_from = "data")
   
+# no usefull data after row 39
 Worldbank2 <- Worldbank2_raw[1:39,] %>%
   mutate(`2022 [YR2022]` = as.numeric(`2022 [YR2022]`),
          `2023 [YR2023]` = as.numeric(`2023 [YR2023]`)) %>%
@@ -22,5 +24,12 @@ Worldbank <- Worldbank1 %>%
   full_join(Worldbank2, by = c("Country Name" = "Country Name", "Country Code" = "Country Code", "Year" = "Year")) %>%
   mutate(across(c(`Country Name`, `Country Code`), ~as.factor(.x))) %>%
   mutate(Year = as.ordered(Year))
+
+Worldbank <- Worldbank %>%
+  filter(Year == 2014) %>%
+  select(`Country Name`, `Population, total`) %>%
+  rename(Pop.Index2014 = `Population, total`) %>%
+  full_join(Worldbank, by = "Country Name") %>%
+  mutate(Pop.Index2014 = round(`Population, total` * 100 / Pop.Index2014, 2))
 
 readr::write_rds(Worldbank, "Data/cleaned/Worldbank.RDS")
