34e20ba5702cdd8ee8c554433fad03d5c2f97381
AkitsukiYau <yanyu.zhao@campus.lmu.de>
2024-11-11 17:36:39 +0100

    set the group

--- R/Tobacco.R
+++ R/Tobacco.R
@@ -13,7 +13,10 @@ filtered_data <- data %>%
          GDP = `GDP per capita, PPP (constant 2021 international $)`)
 
 filtered_data <- filtered_data %>%
-  filter(!is.na(Tobacco) & !is.na(GDP)& Country != "Qatar")
+  filter(!is.na(Tobacco) & !is.na(GDP) &  Country != "Qatar")
+
+filtered_data <- filtered_data %>%
+  mutate(GDP_group = ifelse(GDP > 20000, "GDP Above 20000", "GDP Below 20000"))
 
 plot1 <- ggplot(filtered_data, aes(x = Tobacco, y = GDP, color = Country)) +
   geom_point(size = 2) +
@@ -23,7 +26,8 @@ plot1 <- ggplot(filtered_data, aes(x = Tobacco, y = GDP, color = Country)) +
        y = "GDP per capita, PPP (constant 2021 international $)",
        title = "GDP vs Adult tobacco use rates") +
   theme_minimal() +
-  theme(legend.title = element_text()) 
+  theme(legend.title = element_text()) +
+  facet_wrap(~ GDP_group)
   
 
 print(plot1)
