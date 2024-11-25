aa4c4b532e5143ceeedeb4408ee6dab095115fa4
AkitsukiYau <yanyu.zhao@campus.lmu.de>
2024-11-25 17:36:38 +0100

    exchange X and Y

--- R/Tobacco.R
+++ R/Tobacco.R
@@ -21,7 +21,7 @@ filtered_data <- data %>%
 filtered_data <- filtered_data %>%
   filter(!is.na(Tobacco) & !is.na(GDP) & Country != "Qatar" & Country != "China")
 
-# Grouping by whether the GDP per capita exceeds 20,000
+# Grouping by whether the GDP over 20,000
 filtered_data <- filtered_data %>%
   mutate(GDP_group = ifelse(GDP > 20000, "GDP per capita Over 20000", "GDP per capita Below 20000"))
 
@@ -30,30 +30,9 @@ labels <- filtered_data %>%
   group_by(Country) %>%
   slice_max(order_by = Year)  
 
-# Plot over 20000
-plot_high_gdp <- ggplot(filtered_data %>% filter(GDP_group == "GDP per capita Over 20000"),
-                        aes(x = Tobacco, y = GDP, color = Country)) +
-  geom_point(size = 1) +
-  geom_smooth(aes(group = Country, color = Country), method = "lm", se = FALSE, linewidth = 0.5) +
-  geom_smooth(method = "lm", se = FALSE, color = "red", size = 0.8) +
-  geom_text_repel(data = labels %>% filter(GDP_group == "GDP per capita Over 20000"),
-                  aes(label = Country),
-                  size = 3, max.overlaps = 10) +  
-  scale_color_manual(values = colors_named) +
-  labs(x = "Prevalence of current tobacco use (% of adults)",
-       y = "GDP per capita, PPP (constant 2021 international $)",
-       title = "GDP per capita Over 20000") +
-  theme_minimal() +
-  theme(
-    legend.position = "none",  
-    panel.grid.major = element_line(color = "gray80"),
-    panel.grid.minor = element_line(color = "gray85")
-  ) +
-  xlim(0, 60)
-
 # Plot under 20000
 plot_low_gdp <- ggplot(filtered_data %>% filter(GDP_group == "GDP per capita Below 20000"),
-                       aes(x = Tobacco, y = GDP, color = Country)) +
+                       aes(x = GDP, y = Tobacco, color = Country)) +
   geom_point(size = 1) +
   geom_smooth(aes(group = Country, color = Country), method = "lm", se = FALSE, linewidth = 0.5) +
   geom_smooth(method = "lm", se = FALSE, color = "red", size = 0.8) +
@@ -61,18 +40,40 @@ plot_low_gdp <- ggplot(filtered_data %>% filter(GDP_group == "GDP per capita Bel
                   aes(label = Country),
                   size = 3, max.overlaps = 10) +  
   scale_color_manual(values = colors_named) +
-  labs(x = "Prevalence of current tobacco use (% of adults)",
-       y = "GDP per capita, PPP (constant 2021 international $)",
-       title = "GDP per capita Below 20000") +
+  labs(x = "GDP per capita, PPP (constant 2021 international $)",
+       y = "Prevalence of current tobacco use (% of adults)",
+       title = "GDP per capita Under 20000") +
   theme_minimal() +
   theme(
     legend.position = "none",  
     panel.grid.major = element_line(color = "gray80"),
     panel.grid.minor = element_line(color = "gray85")
   ) +
-  xlim(0, 60)
+  ylim(0, 60)
 
-final_plot <- (plot_high_gdp | plot_low_gdp) +
+# Plot over 20000
+plot_high_gdp <- ggplot(filtered_data %>% filter(GDP_group == "GDP per capita Over 20000"),
+                        aes(x = GDP, y = Tobacco, color = Country)) +
+  geom_point(size = 1) +
+  geom_smooth(aes(group = Country, color = Country), method = "lm", se = FALSE, linewidth = 0.5) +
+  geom_smooth(method = "lm", se = FALSE, color = "red", size = 0.8) +
+  geom_text_repel(data = labels %>% filter(GDP_group == "GDP per capita Over 20000"),
+                  aes(label = Country),
+                  size = 3, max.overlaps = 10) +  
+  scale_color_manual(values = colors_named) +
+  labs(x = "GDP per capita, PPP (constant 2021 international $)",
+       y = NULL,
+       title = "GDP per capita Over 20000") +
+  theme_minimal() +
+  theme(
+    legend.position = "none",  
+    panel.grid.major = element_line(color = "gray80"),
+    panel.grid.minor = element_line(color = "gray85")
+  ) +
+  ylim(0, 60)
+
+
+final_plot <- (plot_low_gdp | plot_high_gdp) +
   plot_annotation(title = "Relationship Between GDP Per Capita and Tobacco Usage Prevalence") 
 
 print(final_plot)
